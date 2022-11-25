1cec654535cbc13aac009b930b6189e09a33fece
Akahad <aksahad234567@gmail.com>
2022-11-25 09:39:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
+
+// jnQsvQ5T8Z13wOcM
+// Resale
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_Name}:${process.env.USER_PASS}@cluster0.xuxoczf.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const catagoryCollction = client.db('Resale').collection('catagory')
+
+
+async function run() {
+  try {
+    app.get('/catagory', async (req, res) => {
+      const qurey = {}
+      const result = await catagoryCollction.find(qurey).toArray()
+      res.send(result)
+
+    })
+
+
+
+  }
+  finally {
+
+  }
+
+}
+run().catch(error => console.error(error))
+
+
+
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('helo Word')
+})
+app.listen(port, () => {
+  console.log('server is running')
+})
